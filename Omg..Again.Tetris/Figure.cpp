777410055a060f337ca98dd63ac84991a968a7f8
Goosev <Crosswayle@gmail.com>
2018-07-20 04:14:40 +0300

    Finished kourse work without files sort

---
+++ Omg..Again.Tetris/Figure.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+//class Figure
+//{
+//private:
+//	COORD currentCoords; // Coords of figure matrix in field(glass) matrix
+//	int stepsCount = 0;
+//	int side = 0;
+//public:
+//
+//	void virtual figureElementsInit(int figurePosition = 0) = 0;
+//	virtual vector<COORD>& GetFigureElements() = 0;
+//	virtual void clearFigureElements() = 0;
+//	int& GetStepsCount()
